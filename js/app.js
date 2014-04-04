47f1284b4b94f12d6bfb66ee61193b86d68ca352
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-04-04 08:37:00 +0200

    update progress from the second meetup

--- js/app.js
+++ js/app.js
@@ -7,14 +7,9 @@ App.Router.reopen({
 });
 
 App.Router.map(function(){
-  this.resource("slides");
+  this.resource("slides", {path: "slides/:id"});
 });
 
-App.ApplicationRoute = Em.Route.extend({
-  redirect: function(){
-    this.transitionTo("slides");
-  }
-})
 
 App.Converter = new Markdown.Converter();
 
@@ -31,29 +26,77 @@ App.Slide = Ember.Object.extend({
 App.Slide.reopenClass({
   findAll: function(){
     var self = this;
-    return Em.$.get("/slides.json").then(function(response){
-      var slides = Em.A();
-      response.forEach(function(child){
-        var slide = App.Slide.create({
-          location: child.location,
-          id: child.id
-        });
-        slides.pushObject(slide);
+    if (this.slides) {
+      return new Em.RSVP.Promise(function(resolve, reject){
+        resolve(self.slides);
       });
-      return slides;
+    } else {
+      return Em.$.get("/slides.json").then(function(response){
+        var slides = Em.A();
+        response.forEach(function(child){
+          var slide = App.Slide.create({
+            location: child.location,
+              id: child.id
+          });
+          slides.pushObject(slide);
+        });
+        self.slides = slides;
+        return slides;
+      });
+    }
+  },
+  find: function(id){
+    return App.Slide.findAll().then(function(slides){
+      return slides.findBy("id", id);
     });
   }
 });
 
+App.ApplicationRoute = Em.Route.extend({
+  activate: function(){
+    console.log("inside application");
+  }
+})
+
+App.IndexRoute = Em.Route.extend({
+  redirect: function(){
+    console.log("inside index");
+    this.transitionTo("slides", 1);
+  }
+})
+
 App.SlidesRoute = Em.Route.extend({
-  model: function(){
-    return App.Slide.findAll().then(function(slides){
-      return slides[0];
-    });
+  model: function(params){
+    var id = parseInt(params.id);
+    return App.Slide.find(id);
   },
   afterModel: function(model){
     return model.loadMarkdown().then(function(){
       return model;
     });
+  },
+  actions: {
+    switchSlide: function(direction){
+      var id = parseInt(this.get("controller.model.id"));
+      if (direction == "left"){
+        this.transitionTo("slides", App.Slide.find(id - 1));
+      }
+      if (direction == "right"){
+        this.transitionTo("slides", App.Slide.find(id + 1));
+      }
+    }
+  }
+});
+
+App.SlidesView = Em.View.extend({
+  didInsertElement: function(){
+    this.$("body").focus();
+  },
+  keyDown: function(event){
+    var direction = {
+      37: "left",
+      39: "right"
+    };
+    this.get("controller").send("switchSlide", direction[event.keyCode]);
   }
 });
